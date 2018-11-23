06c5843707a6c38255d67f6942fdc6e58b1b1162
Donovan <jetdon1@gmail.com>
2018-11-23 10:53:39 +0200

    Portfolio
    My Portfolio site

--- src/components/resume.js
+++ src/components/resume.js
@@ -29,7 +29,7 @@ class Resume extends Component {
                         <h5>Email</h5>
                         <p>jetdon1@gmail.com</p>
                         <h5>Web</h5>
-                        <p>donSiteAddress</p>
+                        <p>https://donovanbotes.netlify.com</p>
                         <hr style={{borderTop:'3px solid #833fb2',width:'50%'}} />
                     </Cell>
                     <Cell className="resume-right-col" col={8}>
